af7ff028e3fc597d9251f36c5e2285693de7b5bd
Lucky Wirasakti <lucky.30@students.amikom.ac.id>
2019-12-18 13:48:48 +0700

    penambahan modul login firebas

--- app/src/main/java/com/temukangen/amikomhilang/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/temukangen/amikomhilang/ui/dashboard/DashboardFragment.java
@@ -20,16 +20,16 @@ public class DashboardFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+//        dashboardViewModel =
+//                ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_dashboard);
+//        dashboardViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
